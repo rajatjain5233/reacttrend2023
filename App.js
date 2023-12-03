e74cda6b6ca886946f03020dbc067910a6d5b34a
Rajat JAIN <jain.rajat5233@gmail.com>
2023-12-03 02:17:43 -0500

    changes for super trend logic with parsefloat

--- App.js
+++ App.js
@@ -13,85 +13,84 @@ var TI = require('technicalindicators');
 const SMA = require('technicalindicators').SMA;
 
 export default   function App() {
-  const [symbolData, setSymbolData] = useState([]);
-  let limit =300;
-//   const newData = [];
+    const [symbolData, setSymbolData] = useState([]);
+    let limit =300;
 
-  const symbols = ['CHRUSDT', 'BTCUSDT', 'ETHUSDT', 'XRPUSDT', 'LTCUSDT'];
+
+    const symbols = ['CHRUSDT', 'BTCUSDT', 'ETHUSDT', 'XRPUSDT', 'LTCUSDT'];
   
   
 
 
     function calculateSuperTrend(time,highPrices, lowPrices, closePrices, multiplier, length) {
-    // console.log("calculateSuperTrend",highPrices,lowPrices)
-    const hl2Values = calculateHL2(highPrices, lowPrices);
-    const atrValues = calculateATR(highPrices, lowPrices, closePrices, length);
+        // console.log("calculateSuperTrend",highPrices,lowPrices)
+        const hl2Values = calculateHL2(highPrices, lowPrices);
+        const atrValues = calculateATR(highPrices, lowPrices, closePrices, length);
 
-    let basicUpperBand = [];
-    let basicLowerBand = [];
-    let upperBand = [];
-    let lowerBand = [];
-    let superTrend = [];
-    let trendIdentifier=[];
+        let basicUpperBand = [];
+        let basicLowerBand = [];
+        let upperBand = [];
+        let lowerBand = [];
+        let superTrend = [];
+        let trendIdentifier=[];
 
-    for (let i = 0; i < closePrices.length; i++) {
-        basicUpperBand[i] = hl2Values[i] + multiplier * atrValues[i];
-        basicLowerBand[i] = hl2Values[i] - multiplier * atrValues[i];
+        for (let i = 0; i < closePrices.length; i++) {
+            basicUpperBand[i] = hl2Values[i] + multiplier * atrValues[i];
+            basicLowerBand[i] = hl2Values[i] - multiplier * atrValues[i];
 
-        upperBand[i] = (i === 0 || basicUpperBand[i] < upperBand[i - 1] || closePrices[i - 1] > upperBand[i - 1]) ?
-        basicUpperBand[i] :
-        upperBand[i - 1];
+            upperBand[i] = (i === 0 || basicUpperBand[i] < upperBand[i - 1] || closePrices[i - 1] > upperBand[i - 1]) ?
+            basicUpperBand[i] :
+            upperBand[i - 1];
 
-        lowerBand[i] = (i === 0 || basicLowerBand[i] > lowerBand[i - 1] || closePrices[i - 1] < lowerBand[i - 1]) ?
-        basicLowerBand[i] :
-        lowerBand[i - 1];
+            lowerBand[i] = (i === 0 || basicLowerBand[i] > lowerBand[i - 1] || closePrices[i - 1] < lowerBand[i - 1]) ?
+            basicLowerBand[i] :
+            lowerBand[i - 1];
 
-        const isUpTrend = closePrices[i] > lowerBand[i];
+            const isUpTrend = closePrices[i] > lowerBand[i];
 
-        const trendDirection = (i < length) ? "isDownTrend" :
-        (superTrend[i - 1] === upperBand[i - 1]) ?
-        (closePrices[i] > upperBand[i] ? "isUpTrend" : "isDownTrend") :
-        (closePrices[i] < lowerBand[i] ? "isDownTrend" : "isUpTrend");
-        
-        let trender=[];
-        trender.push(time[i],trendDirection);
+            const trendDirection = (i < length) ? "isDownTrend" :
+            (superTrend[i - 1] === upperBand[i - 1]) ?
+            (closePrices[i] > upperBand[i] ? "isUpTrend" : "isDownTrend") :
+            (closePrices[i] < lowerBand[i] ? "isDownTrend" : "isUpTrend");
+            
+            let trender=[];
+            trender.push(time[i],trendDirection);
 
-        trendIdentifier.push(trender);
-        superTrend[i] = (trendDirection === "isUpTrend") ? lowerBand[i] : upperBand[i];
-    }
-    // console.log("trendIdentifier",trendIdentifier);
+            trendIdentifier.push(trender);
+            superTrend[i] = (trendDirection === "isUpTrend") ? lowerBand[i] : upperBand[i];
+        }
+        // console.log("trendIdentifier",trendIdentifier);
 
-    return trendIdentifier;
+        return trendIdentifier;
     }
 
     function calculateHL2(highPrices, lowPrices) {
-    return highPrices.map((high, i) => (high + lowPrices[i]) / 2);
+      return highPrices.map((high, i) => (parseFloat(high) + parseFloat(lowPrices[i])) / 2);
     }
 
     function calculateATR(highPrices, lowPrices, closePrices, length) {
-    const trValues = calculateTR(highPrices, lowPrices, closePrices);
-    const atrValues = [trValues[0]];
+      const trValues = calculateTR(highPrices, lowPrices, closePrices);
+      const atrValues = [trValues[0]];
 
-    for (let i = 1; i < closePrices.length; i++) {
+      for (let i = 1; i < closePrices.length; i++) {
         atrValues[i] = (atrValues[i - 1] * (length - 1) + trValues[i]) / length;
-    }
-    
+      }
 
-    return atrValues;
+      return atrValues;
     }
 
     function calculateTR(highPrices, lowPrices, closePrices) {
-    const trValues = [];
+      const trValues = [];
 
-    for (let i = 1; i < closePrices.length; i++) {
-        const highLowDiff = highPrices[i] - lowPrices[i];
-        const highCloseDiff = Math.abs(highPrices[i] - closePrices[i - 1]);
-        const lowCloseDiff = Math.abs(lowPrices[i] - closePrices[i - 1]);
+      for (let i = 1; i < closePrices.length; i++) {
+        const highLowDiff = parseFloat(highPrices[i]) - parseFloat(lowPrices[i]);
+        const highCloseDiff = Math.abs(parseFloat(highPrices[i]) - parseFloat(closePrices[i - 1]));
+        const lowCloseDiff = Math.abs(parseFloat(lowPrices[i]) - parseFloat(closePrices[i - 1]));
 
         trValues.push(Math.max(highLowDiff, highCloseDiff, lowCloseDiff));
-    }
+      }
 
-    return trValues;
+      return trValues;
     }
 
 
@@ -117,6 +116,7 @@ export default   function App() {
 
         const fetchData = async () => {
           let newData = [];  
+          let count=0
           
           
 
@@ -161,13 +161,19 @@ export default   function App() {
               symbol_Individual_data.signal=superTrendValues[limit-1][1];
               console.log(symbol_Individual_data);
               newData.push(symbol_Individual_data);
+              count++;
+              if(count==symbols.length-1){
+                console.log("newData",newData)
+                setSymbolData(newData);
+              }
+                
 
                         
             })
 
           }
-          // console.log("newData",newData)
-          setSymbolData(newData);
+          
+          
         }  
 
         fetchData()
@@ -187,7 +193,11 @@ export default   function App() {
       {symbolData.map(symbol => (
         <p key={symbol.pair}>
           {
-             symbol.pair
+            symbol.pair+"   "
+          }
+
+          {
+            symbol.signal
           }
         </p>
       ))}
